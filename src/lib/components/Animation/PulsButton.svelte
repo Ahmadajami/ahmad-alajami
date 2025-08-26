45963aadd9771f3f73a64393a17746ad203149b0
Ahmad Alajami <ahmad_2000_aj@hotmail.com>
2025-08-26 13:17:25 +0300

    Init

---
+++ src/lib/components/Animation/PulsButton.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	interface Props {
+		pulseColor?: string;
+		duration?: string;
+		class?: string;
+		onclick?: () => void;
+		children?: import('svelte').Snippet;
+	}
+
+	let {
+		pulseColor = 'var(--secondary)',
+		duration = '1.5s',
+		class: className = '',
+		onclick,
+		children
+	}: Props = $props();
+</script>
+
+<button
+	class={cn(
+		'relative flex cursor-pointer items-center justify-center rounded-lg bg-primary px-4 py-2 text-center text-black',
+		className
+	)}
+	style:--pulse-color={pulseColor}
+	style:--duration={duration}
+	{onclick}
+>
+	<div class="relative z-10">
+		{#if children}{@render children()}{:else}Button{/if}
+	</div>
+	<div
+		class="absolute top-1/2 left-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-lg bg-inherit"
+	></div>
+</button>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Ahmad alajami - Front-End Developer</title>
+</svelte:head>
+
+<main class="container mx-auto overflow-hidden pt-12">
+	{@render children?.()}
+</main>
